82fc9ae28a33068cb3090bd8c2cbfaa8d6f7ee5d
Kemal Anıl Güvenç <82261358+Anilgvnc@users.noreply.github.com>
2025-08-09 11:44:42 +0300

    Header changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
-import ReactDOM from "react-dom/client";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
 import SingleInput from "./pages/SingleInput";
@@ -8,27 +7,20 @@ import MultipleInput from "./pages/MultipleInput";
 import GenreInput from "./pages/GenreInput";
 import About from "./pages/About";
 import NoPage from "./pages/NoPage";
-import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="single" element={<SingleInput />} />
-          <Route path="multiple" element={<MultipleInput />} />
-          <Route path="genre" element={<GenreInput />} />
-          <Route path="about" element={<About />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="single" element={<SingleInput />} />
+        <Route path="multiple" element={<MultipleInput />} />
+        <Route path="genre" element={<GenreInput />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NoPage />} />
+      </Route>
+    </Routes>
   );
 };
 
 export default App;
-
-const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<App />);
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,27 +1,32 @@
-import { Outlet, Link } from "react-router";
+import { Outlet, Link } from "react-router-dom";
 import "../constants/Layout.css";
 import Logo from "../assets/logo.png";
+import Footer from "../components/Footer";
 
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link className="logo-container" to="/">
-              <img src={Logo} className="logo" alt="ShowGenius" />
-              <h3>ShowGenius</h3>
-            </Link>
-          </li>
-          <li style={{ float: "right" }}>
-            <Link className="link" to="/about">
-              About
-            </Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Outlet />
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link className="logo-container" to="/">
+                <img src={Logo} className="logo" alt="ShowGenius" />
+                <h3>ShowGenius</h3>
+              </Link>
+            </li>
+            <li style={{ float: "right" }}>
+              <Link className="link" to="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </>
   );
 };
